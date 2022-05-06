5bbf0040f6624578015dc76e029cde5c6b8bd5d4
MicahHarlan <81989762+MicahHarlan@users.noreply.github.com>
2022-05-06 14:53:33 -0400

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { XyzComponent } from './xyz/xyz.component';
+import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XyzComponent,
+	CoursesComponent,
+ CourseComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/courses.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+	selector: 'courses',
+	template: '<h2>Courses</h2>' //CAN BE A SEPERATE FILE
+}) 
+export class CoursesComponent {
+
+}
